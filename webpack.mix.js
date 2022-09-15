886ee428d4c8ee5f9b75a72d49fd99d1d1b035ea
nlloydw <nlloydw61@gmail.co.uk>
2022-09-15 14:47:38 +0100

    updated webpack build rules

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
+    .options({ processCssUrls: false })
     .postCss('resources/css/app.css', 'public/css', [
         require("tailwindcss"),
     ]);
