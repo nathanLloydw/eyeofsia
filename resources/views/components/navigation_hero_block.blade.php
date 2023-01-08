7e9670da2d8f439444bbfa95d66c129f2971df8d
nlloydw <nlloydw61@gmail.co.uk>
2023-01-08 18:13:09 +0000

    responsive fix

--- resources/views/components/navigation_hero_block.blade.php
+++ resources/views/components/navigation_hero_block.blade.php
@@ -1,4 +1,4 @@
-<div id="navigation-hero-block" class="container mx-auto px-14 py-6 relative">
+<div id="navigation-hero-block" class="container mx-auto px-10 lg:px-14 py-6 relative">
 
     <div class="absolute flex justify-center items-center w-full h-[83vh] my-auto z-10 left-0">
         <div class="h-fit text-[65px] sm:text-[75px] md:text-[100px] lg:text-[165px] bg-clip-text text-transparent opacity-50 bg-gradient-to-br from-cyan-300 to-rose-600">EYE OF SIA</div>
