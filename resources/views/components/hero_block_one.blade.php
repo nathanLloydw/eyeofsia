cfdef3b0c20909846884b895740c939a04448d55
nlloydw <nlloydw61@gmail.co.uk>
2023-01-23 13:54:37 +0000

    hero_block_one.blade.php bug fix

--- resources/views/components/hero_block_one.blade.php
+++ resources/views/components/hero_block_one.blade.php
@@ -1,7 +1,7 @@
 <section class="container mx-auto lg:flex py-6 px-6 lg:px-14">
 
     @if($component['image_location']->value() == 'left')
-        <div @class(["w-full lg:px-8 flex items-center",
+        <div @class(["w-full lg:px-8 flex items-center shrink-0",
                      'lg:w-1/2'=>$component['block_ratio'] == '50-50',
                      'lg:w-2/3'=>$component['block_ratio'] == '66-33',
                      'lg:w-3/4'=>$component['block_ratio'] == '75-25'])>
@@ -27,7 +27,7 @@
     </div>
 
     @if($component['image_location']->value() == 'right')
-        <div @class(["w-full lg:px-8 flex items-center",
+        <div @class(["w-full lg:px-8 flex items-center shrink-0",
                      'lg:w-1/2'=>$component['block_ratio'] == '50-50',
                      'lg:w-1/3'=>$component['block_ratio'] == '66-33',
                      'lg:w-1/4'=>$component['block_ratio'] == '75-25'])>
