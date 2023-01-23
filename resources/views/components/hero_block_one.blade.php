a21732736ceb98be174c9abb21289415a08b3146
nlloydw <nlloydw61@gmail.co.uk>
2023-01-23 14:03:18 +0000

    added more options to the hero_block_one.blade.php component

--- resources/views/components/hero_block_one.blade.php
+++ resources/views/components/hero_block_one.blade.php
@@ -5,14 +5,13 @@
                      'lg:w-1/2'=>$component['block_ratio'] == '50-50',
                      'lg:w-2/3'=>$component['block_ratio'] == '66-33',
                      'lg:w-3/4'=>$component['block_ratio'] == '75-25'])>
-            <img class="m-auto" src="{{ $component['hero_image']['permalink'] }}">
+            <img @class(["m-auto",'rounded-lg'=>$component['rounded_image_borders']]) src="{{ $component['hero_image']['permalink'] }}">
         </div>
     @endif
 
     <div class="w-full lg:w-auto lg:px-10 m-auto flex flex-col items-center py-6">
         <div class="lg:flex justify-center items-center flex-col mb-5 space-y-2">
-            {!! $component['header'] !!}
-            <p class="mt-4">{!! $component['description'] !!}</p>
+            {!! $component['context'] !!}
         </div>
 
         <div class="flex space-x-4 justify-center items-center">
@@ -31,7 +30,7 @@
                      'lg:w-1/2'=>$component['block_ratio'] == '50-50',
                      'lg:w-1/3'=>$component['block_ratio'] == '66-33',
                      'lg:w-1/4'=>$component['block_ratio'] == '75-25'])>
-            <img class="m-auto" src="{{ $component['hero_image']['permalink'] }}">
+            <img @class(["m-auto",'rounded-lg'=>$component['rounded_image_borders']]) src="{{ $component['hero_image']['permalink'] }}">
         </div>
     @endif
 
