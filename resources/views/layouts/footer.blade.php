19c03d91d68492f3861b7543a9bfebecf1783a48
nlloydw <nlloydw61@gmail.co.uk>
2023-01-26 09:56:23 +0000

    added a nav option to show a small or large footer nav

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,35 +2,37 @@
 
 <footer @class(["bg-white shadow-2xl","sticky bottom-0 z-50"=>$navigation_settings['footer_sticky']])>
     <div class="container mx-auto px-14 py-6">
-        <div class="md:flex md:justify-between">
-            <div class="mb-6 md:mb-0">
-                <a href="#" class="flex items-center px-2 w-max">
-                    @if($navigation_settings['footer_company_logo'])
-                        <img src="{{ $navigation_settings['footer_company_logo']['permalink'] }}" class="mr-3 h-8">
-                    @endif
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap">{{ $company_details['company_name'] }}</span>
-                </a>
-            </div>
-            <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-                @foreach(Statamic::tag('nav:footer_navigation') as $item)
-
-                    <div>
-                        <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">{{ $item['title'] }}</h2>
-                        @if(count($item['children']) > 0)
-                            <ul class="text-gray-600">
-                                @foreach($item['children'] as $child_item)
-                                    <li class="mb-4 hover:text-primary">
-                                        <a href="{{ $child_item['url'] }}" class="hover:text-primary">{{ $child_item['title'] }}</a>
-                                    </li>
-                                @endforeach
-                            </ul>
+        @if($navigation_settings['show_footer_navigation'])
+            <div class="md:flex md:justify-between">
+                <div class="mb-6 md:mb-0">
+                    <a href="#" class="flex items-center px-2 w-max">
+                        @if($navigation_settings['footer_company_logo'])
+                            <img src="{{ $navigation_settings['footer_company_logo']['permalink'] }}" class="mr-3 h-8">
                         @endif
-                    </div>
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap">{{ $company_details['company_name'] }}</span>
+                    </a>
+                </div>
+                <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                    @foreach(Statamic::tag('nav:footer_navigation') as $item)
 
-                @endforeach
+                        <div>
+                            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">{{ $item['title'] }}</h2>
+                            @if(count($item['children']) > 0)
+                                <ul class="text-gray-600">
+                                    @foreach($item['children'] as $child_item)
+                                        <li class="mb-4 hover:text-primary">
+                                            <a href="{{ $child_item['url'] }}" class="hover:text-primary">{{ $child_item['title'] }}</a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            @endif
+                        </div>
 
+                    @endforeach
+
+                </div>
             </div>
-        </div>
+        @endif
         <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8">
         <div class="sm:flex sm:items-center sm:justify-between">
         <span class="text-sm text-gray-500 sm:text-center">© 2022 <a href="#" class="hover:text-primary">{{ $company_details['company_name'] }}™</a>. All Rights Reserved.
